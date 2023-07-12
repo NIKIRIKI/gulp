cff7dffdd520a99855a35e0e85fec40afb192d81
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2023-07-12 11:07:53 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,86 @@ const cleanCSS = require("gulp-clean-css");
 const cleanJS = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const imagemin = require("gulp-imagemin");
-const cache = require("gulp-cache");
+const newer = require("gulp-newer");
 const webp = require("gulp-webp");
 const clean = require("gulp-clean");
+const avif = require("gulp-avif");
+const makeDir = require("make-dir");
+const fs = require("fs");
+const svgSprite = require("gulp-svg-sprite");
+const fonter = require("gulp-fonter");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const include = require("gulp-include");
+
+function pages() {
+  return src("app/pages/*.html")
+    .pipe(
+      include({
+        includePaths: "app/components",
+      })
+    )
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
+}
+
+// Создание начальной структуры проекта
+function start() {
+  (async () => {
+    const paths = await Promise.all([
+      makeDir("app"),
+      makeDir("app/components"),
+      makeDir("app/css"),
+      makeDir("app/img"),
+      makeDir("app/img/src"),
+      makeDir("app/img/dist"),
+      makeDir("app/js"),
+      makeDir("app/scss"),
+      makeDir("app/pages"),
+      makeDir("app/fonts"),
+      makeDir("app/fonts/src"),
+      makeDir("app/fonts/dist"),
+      makeDir("public"),
+    ]);
+  })();
+  fs.writeFile("app/pages/index.html", "", (err) => {
+    if (err) throw err;
+  });
+  fs.writeFile("app/js/main.js", "", (err) => {
+    if (err) throw err;
+  });
+  fs.writeFile("app/scss/style.scss", "", (err) => {
+    if (err) throw err;
+  });
+}
+
+// Обработка спрайтов
+function sprite() {
+  return src("app/img/dist/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg",
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest("app/img/dist"));
+}
+
+// Обработка шрифтов
+function fonts() {
+  return src("app/fonts/src/**/*.*")
+    .pipe(
+      fonter({
+        formats: ["woff", "ttf"],
+      })
+    )
+    .pipe(src("app/fonts/dist/**/*.ttf"))
+    .pipe(ttf2woff2())
+    .pipe(dest("app/fonts/dist/"));
+}
 
 // Обработка стилей
 function styles() {
@@ -37,21 +114,35 @@ function scripts() {
 
 // Обработка изображений
 function images() {
-  return src("app/img/**/*.*")
-    .pipe(
-      cache(
-        imagemin({
-          interlaced: true,
-        })
-      )
-    )
+  return src(["app/img/src/**/*.*", "!app/img/src/**/*.svg"])
+    .pipe(newer("app/img/dist"))
+    .pipe(avif({ quality: 50 }))
+    .pipe(src(["app/img/src/**/*.*", "!app/img/src/**/*.svg"]))
+    .pipe(newer("app/img/dist"))
     .pipe(webp())
-    .pipe(dest("app/img/"));
+    .pipe(src(["app/img/src/**/*.*", "!app/img/src/**/*.svg"]))
+    .pipe(newer("app/img/dist"))
+    .pipe(imagemin())
+    .pipe(dest("app/img/dist/"));
+}
+
+// Конвертация изображения в формат webp
+function convertWebp() {
+  return src(["app/img/src/**/*.*", "!app/img/src/**/*.svg"])
+    .pipe(webp())
+    .pipe(dest("app/img/dist/"));
+}
+
+// Конвертация изображения в формат avif
+function convertAvif() {
+  return src(["app/img/src/**/*.*", "!app/img/src/**/*.svg"])
+    .pipe(avif({ quality: 50 }))
+    .pipe(dest("app/img/dist/"));
 }
 
 // Очистка директории с изображениями в папке app
-function cleanImagesApp() {
-  return cleanDirectory("app/img/");
+function cleanImages() {
+  return cleanDirectory("app/img/dist/");
 }
 
 // Очистка директории с CSS-файлами в папке app
@@ -73,13 +164,9 @@ function cleanScssApp() {
 function cleanDirectory(directory) {
   return src(directory).pipe(clean()).pipe(dest(directory));
 }
-
-// Отслеживание изменений в файлах
-function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/img/**/*.*"], images);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+// Очистка директории public перед сборкой проекта
+function cleanPublic() {
+  return cleanDirectory("public/");
 }
 
 // Запуск локального сервера для разработки
@@ -91,6 +178,16 @@ function browsersync() {
   });
 }
 
+// Отслеживание изменений в файлах
+function watching() {
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/components/*", "app/pages/*"], pages);
+  watch(["app/img/src"], images);
+  watch(["app/fonts/src"], fonts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
+}
+
 // Сборка проекта для публикации
 function building() {
   return src(
@@ -98,29 +195,30 @@ function building() {
       "app/css/style.min.css",
       "app/js/main.min.js",
       "app/**/*.html",
-      "app/img/**/*.*",
+      "app/img/dist",
+      "app/fonts/dist/**/*.*",
+      "!app/img/dist/**/*.svg",
+      "app/img/dist/**/sprite.svg",
     ],
     { base: "app" }
   ).pipe(dest("public"));
 }
 
-// Очистка директории public перед сборкой проекта
-function cleanPublic() {
-  return cleanDirectory("public/");
-}
-
+// Экспорт функций
 exports.styles = styles;
 exports.images = images;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.build = series(cleanPublic, building);
-exports.cleanImages = cleanImagesApp;
+exports.cleanImages = cleanImages;
 exports.cleanPublic = cleanPublic;
-exports.cleanApp = series(
-  cleanImagesApp,
-  cleanCssApp,
-  cleanJsApp,
-  cleanScssApp
-);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.cleanApp = series(cleanImages, cleanCssApp, cleanJsApp, cleanScssApp);
+exports.cleanDirectory = cleanDirectory;
+exports.convertWebp = convertWebp;
+exports.convertAvif = convertAvif;
+exports.default = parallel(styles, scripts, browsersync, pages, watching);
+exports.start = start;
+exports.sprite = sprite;
+exports.fonts = fonts;
+exports.pages = pages;
